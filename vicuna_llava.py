e029f53a001a0b35e83adbc9c3094b35ad6bec2a
cjschul <cjschul@umich.edu>
2024-11-19 18:01:14 -0500

    vicuna_llava.py: 1. minor corrections to the vicuna_llava class

--- vicuna_llava.py
+++ vicuna_llava.py
@@ -1,4 +1,4 @@
-from transformers import LlamaModel, PreTrainedModel
+from transformers import LlamaForCausalLM, PreTrainedModel
 from transformers.tokenization_utils_base import BatchEncoding
 from transformers.modeling_outputs import BaseModelOutputWithPooling
 import torch.nn as nn
@@ -7,7 +7,7 @@ import torch
 class vicuna_llava(PreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
-        self.model = LlamaModel(config)
+        self.model = LlamaForCausalLM(config)
         self.im_embedding = nn.Linear(768, 4096)
         self.pr_embedding = self.model.get_input_embeddings()
         self.model.set_input_embeddings(nn.Identity())
@@ -19,8 +19,11 @@ class vicuna_llava(PreTrainedModel):
         projected_im = self.im_embedding(clip_encoded_im['last_hidden_state'])
         embedded_pr = self.pr_embedding(tokenized_pr_input_ids)
 
-        im_and_pr_input_ids = torch.cat((projected_im, embedded_pr), dim=1)
+        im_and_pr_input_ids = torch.cat((embedded_pr[:,[0],:],projected_im, embedded_pr[:,1:,:]), dim=1)
         im_and_pr_attention_mask = torch.cat((torch.ones(1,projected_im.size()[1]), tokenized_pr_attention_mask),dim=1)
 
-        return self.model.forward(input_ids=im_and_pr_input_ids,attention_mask=im_and_pr_attention_mask)            
-        
+        llama_output = self.model.forward(input_ids=im_and_pr_input_ids,attention_mask=im_and_pr_attention_mask)
+
+        # logits = llama_output['last_hidden_state']
+
+        return llama_output
