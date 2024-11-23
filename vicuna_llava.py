a3c24ab5da73c4b512ab976c2e45eef3f5d84bcb
cjschul <cjschul@umich.edu>
2024-11-23 10:09:42 -0500

    1. llava_init_init_wip     a. minor changes to training loop to coincide with model changes 2. vicuna_llava.py     a. added self.tokenize, self.vision_tower,self.generate     b. slight changes to self.forward to make for cleaner code     c. gonna try to grab pretrained llava weights to make sure this is right

--- vicuna_llava.py
+++ vicuna_llava.py
@@ -1,22 +1,38 @@
-from transformers import AutoModelForCausalLM, LlamaForCausalLM, PreTrainedModel
+from transformers import AutoModelForCausalLM, LlamaForCausalLM, PreTrainedModel, CLIPImageProcessor, CLIPVisionModel, AutoTokenizer
 from transformers.tokenization_utils_base import BatchEncoding
 from transformers.modeling_outputs import BaseModelOutputWithPooling
 import torch.nn as nn
 import torch
 from torch.utils.data import Dataset
+from torch import Tensor
 import json
 import os
 from torchvision.io import read_image
+import numpy as np
 
 class vicuna_llava(LlamaForCausalLM):
-    def __init__(self, config):
+    def __init__(self, config, llmURL="lmsys/vicuna-7b-v1.5", clipvisionmodelURL="openai/clip-vit-large-patch14"):
         super().__init__(config)
 
         # grab pretrained vicuna-7B-v1.5 for main LM functionality
-        self.model = AutoModelForCausalLM.from_pretrained("lmsys/vicuna-7b-v1.5")
+        self.model = AutoModelForCausalLM.from_pretrained(llmURL)
+
+        # vision tower
+        self.im_processor = CLIPImageProcessor.from_pretrained(clipvisionmodelURL)
+        self.clipvisionmodel = CLIPVisionModel.from_pretrained(clipvisionmodelURL)
+
+        # tokenizer
+        self.tokenizer = AutoTokenizer.from_pretrained(llmURL)
+        self.special_tokens_dict = {"additional_special_tokens": ["<image>", "###"]}
+        self.num_added_tokens = self.tokenizer.add_special_tokens(self.special_tokens_dict)
+        if self.num_added_tokens > 0:
+            with torch.no_grad():
+                self.model.resize_token_embeddings(len(self.tokenizer))
+                self.model.config.vocab_size = len(self.tokenizer)
+
 
         # construct embedding layer for image for projecting to vicuna's word embedding layer
-        self.im_embedding = nn.Linear(768, 4096)
+        self.im_embedding = nn.Linear(1024, 4096)
 
         # pull vicuna's word embedding layer out so prompt can be concatenated with im_embedding output
         self.pr_embedding = self.model.get_input_embeddings()
@@ -25,16 +41,32 @@ class vicuna_llava(LlamaForCausalLM):
         self.model.set_input_embeddings(nn.Identity())
         self.lm_head = self.model.lm_head
 
-    def forward(self, clip_encoded_im:BaseModelOutputWithPooling, 
-                tokenized_pr:BatchEncoding,
-                batch_size=None):
-        
+    def tokenize(self, prompt):
+        return self.tokenizer(prompt, return_tensors='pt',
+                                        padding='longest',
+                                        max_length=self.tokenizer.model_max_length,
+                                        truncation=True)
+
+    def vision_tower(self, image):
+        proc_im = self.im_processor(image, return_tensors='pt')
+        return self.clipvisionmodel(**proc_im).last_hidden_state
+
+    def forward(self, image:Tensor, 
+                prompt:str,
+                batch_size=1):
+        with torch.no_grad():
+            # tokenize prompt
+            tokenized_pr = self.tokenize(prompt)
+
+            # process image, extract features
+            clip_encoded_im = self.vision_tower(image)
+    
         # grab token ids, attention mask from input prompts
         tokenized_pr_input_ids = tokenized_pr['input_ids']
         tokenized_pr_attention_mask = tokenized_pr['attention_mask']
 
         # project input image to vicuna v1.5 7B's word embedding layer
-        projected_im = self.im_embedding(clip_encoded_im['last_hidden_state'])
+        projected_im = self.im_embedding(clip_encoded_im)
         embedded_pr = self.pr_embedding(tokenized_pr_input_ids)
 
         # concatenate our embedded prompt and projected image features
@@ -56,6 +88,29 @@ class vicuna_llava(LlamaForCausalLM):
         llama_output = torch.softmax(llama_output.logits, dim=1)
 
         return llama_output
+    
+    def generate(self, image:Tensor, prompt:str, max_new_tokens=50, batch_size=1):
+        num_tokens = 0
+        pred_tokens = ''
+        eos_token = self.tokenizer.special_tokens_map['eos_token']
+
+        while num_tokens < max_new_tokens:
+            outs = self.forward(image,prompt+pred_tokens,batch_size=1)
+
+            pred_token_id = torch.argmax(outs[:,-1,:])
+            pred_token = self.tokenizer.decode(pred_token_id)
+
+            pred_tokens+=pred_token
+            if pred_token_id == eos_token:
+                break
+
+            num_tokens+=1
+
+        return pred_tokens
+
+
+
+
 
 class dataset_llava(Dataset):
     def __init__(self, prompts_file:str, images_dir:str):
